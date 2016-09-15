e06051c59b181e325345ed5c98b592281f258f2b
Arnav Gudibande <arnav.gudibande@gmail.com>
2016-09-14 17:56:16 -0700

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import redis.clients.jedis.Jedis;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Jedis jedis = new Jedis("127.0.0.1");
+        while(true) {
+
+            //System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+            System.out.print("\033[H\033[2J");
+            Set<String> names = jedis.keys("*");
+            Iterator<String> it = names.iterator();
+
+            while (it.hasNext()) {
+                String s = it.next();
+                System.out.println(s + ": " + jedis.get(s));
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.exit(0);
+            }
+        }
+    }
+}
